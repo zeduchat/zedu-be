42d8242f13b34decbc1a59b2d6de22d7dac2420b
urizennnn <igamerryt@gmail.com>
2024-08-14 09:09:25 +0100

    fix:fixing wrong syntax

--- internal/models/user.go
+++ internal/models/user.go
@@ -15,7 +15,7 @@ type User struct {
 	Name          string         `gorm:"column:name; type:varchar(255)" json:"name"`
 	Email         string         `gorm:"column:email; type:varchar(255)" json:"email"`
 	IsVerified    bool           `gorm:"column:is_verified; type:bool" json:"is_verified"`
-IsActive      bool            `gorm:"column:is_active; type:bool; default:false" json:"is_active"`
+	IsActive      bool           `gorm:"column:is_active; type:bool; default:false" json:"is_active"`
 	IsOnboarded   bool           `gorm:"column:is_onboarded; type:bool" json:"is_onbarded"`
 	CurrentOrg    uuid.UUID      `gorm:"column:current_org;null; type:uuid" json:"current_org"`
 	Profile       Profile        `gorm:"foreignKey:Userid;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"profile"`
@@ -202,10 +202,6 @@ func (user *User) UpdateUserEmail(db *gorm.DB, req UpdateUserProfileRequest, use
 	userUpdates := User{Email: req.Email}
 
 	result, err := postgresql.UpdateFields(db, &user, userUpdates, "id = ?", userId)
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
 	if err != nil {
 		return err
 	}
