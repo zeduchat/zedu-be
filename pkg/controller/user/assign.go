a9d222c05c1b0b77a404f68cb3930b3ac0f473b5
Ayotola Ajisola <49092282+Ajinx1@users.noreply.github.com>
2024-08-20 08:57:02 +0100

    fix: added permission to user change role

--- pkg/controller/user/assign.go
+++ pkg/controller/user/assign.go
@@ -1,9 +1,11 @@
 package user
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	service "github.com/hngprojects/telex_be/services/user"
 	"github.com/hngprojects/telex_be/utility"
 )
@@ -11,6 +13,18 @@ import (
 func (base *Controller) AssignRoleToUser(c *gin.Context) {
 	userID := c.Param("user_id")
 	roleID := c.Param("role_id")
+
+	if _, err := uuid.Parse(userID); err != nil {
+		rd := utility.BuildErrorResponse(http.StatusBadRequest, "error", "invalid user id format", errors.New("failed to parse user id"), nil)
+		c.JSON(http.StatusBadRequest, rd)
+		return
+	}
+
+	if _, err := uuid.Parse(roleID); err != nil {
+		rd := utility.BuildErrorResponse(http.StatusBadRequest, "error", "invalid role id format", errors.New("failed to parse role id"), nil)
+		c.JSON(http.StatusBadRequest, rd)
+		return
+	}
 
 	userData, err := service.ReplaceUserRole(userID, roleID, base.Db.Postgresql, c)
 	if err != nil {
