a08af9151e6606cc1f761106cbe5b97d8e5ed9cc
Iretoms <iretomiwajonathan@gmail.com>
2024-08-20 13:38:28 +0100

    chore: update email content

--- services/notification/contact_us.go
+++ services/notification/contact_us.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/hngprojects/telex_be/internal/config"
 	"github.com/hngprojects/telex_be/internal/models"
 	"github.com/hngprojects/telex_be/services/send"
 )
@@ -15,6 +16,7 @@ func (n NotificationObject) SendContactUsMail() error {
 		subject              = "Subject: Thank You for Contacting Us😇"
 		templateFileName     = "contact_us.html"
 		baseTemplateFileName = ""
+		configData           = config.GetConfig()
 	)
 
 	err := json.Unmarshal([]byte(n.Notification.Data), &notificationData)
@@ -22,11 +24,19 @@ func (n NotificationObject) SendContactUsMail() error {
 		return fmt.Errorf("error decoding saved notification data, %v", err)
 	}
 
-	data, err := ConvertToMapAndAddExtraData(notificationData, map[string]interface{}{"firstname": thisOrThatStr(notificationData.Name, notificationData.Email), "phone_number": thisOrThatStr(notificationData.PhoneNumber, "")})
+	helpCenterUrl := fmt.Sprintf("%v/help", configData.App.Url)
+
+	data, err := ConvertToMapAndAddExtraData(notificationData, map[string]interface{}{
+		"firstname":       thisOrThatStr(notificationData.Name, notificationData.Email),
+		"phone_number":    notificationData.PhoneNumber,
+		"email":           notificationData.Email,
+		"message":         notificationData.Message,
+		"help_center_url": helpCenterUrl,
+	})
+
 	if err != nil {
 		return fmt.Errorf("error converting data to map, %v, %v", err, strings.Join([]string{err.Error()}, ", "))
 	}
 
 	return send.SendEmail(n.ExtReq, notificationData.Email, subject, templateFileName, baseTemplateFileName, data)
 }
-
