d5ba791c23971e74578c2b9e81e8249ec7245800
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2024-08-24 16:29:24 +0100

    chore: added a notification when password is changed successfully

--- services/auth/password.go
+++ services/auth/password.go
@@ -60,6 +60,15 @@ func UpdateUserPassword(c *gin.Context, req models.ChangePasswordRequestModel, d
 		return nil, http.StatusBadRequest, err
 	}
 
+	resetReq := models.SendOTP{
+		Email:    userDataExist.Email,
+	}
+
+	err = actions.AddNotificationToQueue(storage.DB.Redis, names.SendPasswordChangeConfirmationMail, resetReq)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
 	return &userDataExist, http.StatusOK, nil
 }
 
