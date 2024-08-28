122d6a165417229b6573cd8fe6f05a5828e202fb
cyberguru <cyberguru@cyberguru>
2024-08-28 09:10:51 -0500

    chore: update cors origin

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -11,7 +11,9 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://staging.telex.im,https://telex.im,http://localhost*")
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "https://staging.telex.im")
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "https://telex.im")
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "http://localhost*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
