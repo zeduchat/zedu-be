2828c59c4706cbeeb8651480d58535cc75faff9b
cyberguru <cyberguru@cyberguru>
2024-08-28 09:59:24 -0500

    chore: update cors origin

--- pkg/router/channel.go
+++ pkg/router/channel.go
@@ -19,7 +19,7 @@ func Channels(r *gin.Engine, ApiVersion string, validator *validator.Validate, d
 
 	channelUrl := r.Group(fmt.Sprintf("%v/channels", ApiVersion), middleware.Authorize(db.Postgresql), middleware.CheckIsDeactivated(db.Postgresql), middleware.MonitorFreeSub(db.Postgresql))
 	{
-		channelUrl.POST("/", channel.CreateChannels)
+		channelUrl.POST("", channel.CreateChannels)
 		channelUrl.POST("/:channelId/messages", channel.AddChannelsMsg)
 		channelUrl.PUT("/:channelId/messages", channel.EditChannelsMsg)
 		channelUrl.GET("/:channelId/messages", channel.GetChannelsMsg)
