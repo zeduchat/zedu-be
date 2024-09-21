559a0e9ffa5361556a5ac616f6e6002d5ee0b43f
cyberguru <cyberguru@cyberguru>
2024-09-21 10:58:55 -0500

    Chore: code sanitization

--- internal/models/integration.go
+++ internal/models/integration.go
@@ -228,7 +228,7 @@ func (oi *OrganisationIntegrations) SetIntegrationStatus(db *gorm.DB, status str
 	default:
 		return errors.New("invalid status")
 	}
-	fmt.Println(status, stat, oi.IsActive)
+
 	if status == stat {
 		return errors.New("current status is already set to " + stat)
 	}
--- main.go
+++ main.go
@@ -10,11 +10,15 @@ import (
 	"github.com/hngprojects/telex_be/cronjobs"
 	"github.com/hngprojects/telex_be/external/request"
 	"github.com/hngprojects/telex_be/internal/config"
+	"github.com/hngprojects/telex_be/internal/models"
 	"github.com/hngprojects/telex_be/internal/models/migrations"
 	"github.com/hngprojects/telex_be/internal/models/seed"
+	"github.com/hngprojects/telex_be/pkg/repository/centrifuge"
 	"github.com/hngprojects/telex_be/pkg/repository/storage"
+	"github.com/hngprojects/telex_be/pkg/repository/storage/minio"
 	"github.com/hngprojects/telex_be/pkg/repository/storage/postgresql"
 	"github.com/hngprojects/telex_be/pkg/repository/storage/redis"
+	"github.com/hngprojects/telex_be/pkg/repository/storage/typesense"
 	"github.com/hngprojects/telex_be/pkg/router"
 	"github.com/hngprojects/telex_be/utility"
 )
@@ -26,10 +30,10 @@ func main() {
 	stripe.Key = configuration.Stripe.STRIPE_KEY
 	postgresql.ConnectToDatabase(logger, configuration.Database)
 	redis.ConnectToRedis(logger, configuration.Redis)
-	// minio.ConnectToMinio(logger, configuration.Minio)
-	// centrifuge.NewCentrifugoService(logger, configuration.Centrifuge)
-	// typesense.ConnectToTypeSense(logger, configuration.TypeSense)
-	// models.SetStripeMap(configuration.Stripe)
+	minio.ConnectToMinio(logger, configuration.Minio)
+	centrifuge.NewCentrifugoService(logger, configuration.Centrifuge)
+	typesense.ConnectToTypeSense(logger, configuration.TypeSense)
+	models.SetStripeMap(configuration.Stripe)
 
 	validatorRef := validator.New()
 	db := storage.Connection()
