7465fdd15f382de11fd26c9c2f590c5f2cd144dd
cyberguru <cyberguru@cyberguru>
2024-10-18 04:45:10 -0500

    chore: updated message broadcast for thread

--- internal/models/thread.go
+++ internal/models/thread.go
@@ -26,7 +26,7 @@ type Threads struct {
 	LastReply     time.Time  `json:"last_reply"`
 	AvatarURL     string     `json:"avatar_url"`
 	Type          string     `gorm:"default:thread" json:"type"`
-	Content       string     `gorm:"type:text;index" json:"content"`
+	Content       string     `gorm:"type:text;index" json:"message"`
 	ChannelName   string     `json:"channel_name"`
 	CurrentStatus string     `json:"current_status"`
 	FullName      string     `json:"full_name"`
