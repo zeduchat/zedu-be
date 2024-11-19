02c1e804f8d77d5cd8eada3038fcdc163414f80d
cyberguru <hamzacypher34@gmail.com>
2024-11-19 10:24:47 -0600

    setup elastic db

---
+++ internal/config/elasticdb.go
@@ -0,0 +1,5 @@
+package config
+
+type ElasticDb struct {
+	ElasticEndpoint string
+}
---
+++ internal/models/seed/elastic_index.go
@@ -0,0 +1,31 @@
+package seed
+
+import (
+	"fmt"
+
+	"github.com/elastic/go-elasticsearch/v8"
+
+	"github.com/hngprojects/telex_be/internal/models"
+	"github.com/hngprojects/telex_be/pkg/repository/storage/elastic"
+	"github.com/hngprojects/telex_be/utility"
+)
+
+var Indexs = map[string]interface{}{
+	"threads":  models.Thread_mapping,
+	"messages": models.MessageMapping,
+}
+
+func SeedIndex(logger *utility.Logger, es *elasticsearch.Client) {
+
+	for index, mapping := range Indexs {
+
+		logger.Info("Creating Index for: " + index)
+
+		err := elastic.CreateIndex(es, index, mapping, logger)
+		if err != nil {
+			logger.Error(fmt.Sprintf("An error occurred while indexing: %s, error: %v", index, err))
+		} else {
+			logger.Info("Done indexing: " + index)
+		}
+	}
+}
