74cc07622decfbf3e482809d098f7487b16279f8
cyberguru <hamzacypher34@gmail.com>
2024-11-19 10:33:50 -0600

    setup elastic db

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/stripe/stripe-go/v72"
 
+	"github.com/hngprojects/telex_be/cronjobs"
+	"github.com/hngprojects/telex_be/external/request"
 	"github.com/hngprojects/telex_be/internal/config"
+	"github.com/hngprojects/telex_be/internal/models"
+	"github.com/hngprojects/telex_be/internal/models/migrations"
+	"github.com/hngprojects/telex_be/internal/models/seed"
 	"github.com/hngprojects/telex_be/pkg/repository/centrifuge"
+	"github.com/hngprojects/telex_be/pkg/repository/rabbitmq"
 	"github.com/hngprojects/telex_be/pkg/repository/storage"
 	"github.com/hngprojects/telex_be/pkg/repository/storage/elastic"
+	"github.com/hngprojects/telex_be/pkg/repository/storage/minio"
 	"github.com/hngprojects/telex_be/pkg/repository/storage/postgresql"
 	"github.com/hngprojects/telex_be/pkg/repository/storage/redis"
+	"github.com/hngprojects/telex_be/pkg/repository/storage/typesense"
 	"github.com/hngprojects/telex_be/pkg/router"
 	"github.com/hngprojects/telex_be/utility"
 )
@@ -24,26 +32,26 @@ func main() {
 	stripe.Key = configuration.Stripe.STRIPE_KEY
 	postgresql.ConnectToDatabase(logger, configuration.Database)
 	redis.ConnectToRedis(logger, configuration.Redis)
-	// minio.ConnectToMinio(logger, configuration.Minio)
+	minio.ConnectToMinio(logger, configuration.Minio)
 	centrifuge.NewCentrifugoService(logger, configuration.Centrifuge)
-	// typesense.ConnectToTypeSense(logger, configuration.TypeSense)
-	// models.SetStripeMap(configuration.Stripe)
-	// rabbitmq.QueueClient.QM = rabbitmq.NewQueueManager(configuration.RabbitMQ)
-	// rabbitmq.QueueClient.QM.Start(logger)
+	typesense.ConnectToTypeSense(logger, configuration.TypeSense)
+	models.SetStripeMap(configuration.Stripe)
+	rabbitmq.QueueClient.QM = rabbitmq.NewQueueManager(configuration.RabbitMQ)
+	rabbitmq.QueueClient.QM.Start(logger)
 	elastic.ConnectToElastic(logger, configuration.Elastic)
 
 	validatorRef := validator.New()
 	db := storage.Connection()
 
-	// cronjobs.StartCronJob(request.ExternalRequest{Logger: logger}, *storage.DB, "send-notifications")
+	cronjobs.StartCronJob(request.ExternalRequest{Logger: logger}, *storage.DB, "send-notifications")
 
-	// if configuration.Database.Migrate {
-	// 	migrations.RunAllMigrations(db)
-	// 	seed.SeedRolesAndPermissions(logger, db.Postgresql)
-	// 	seed.SeedPlans(logger, db.Postgresql)
-	// 	seed.SeedIntegrations(logger, db.Postgresql)
-	// 	seed.SeedIndex(logger, db.Elastic)
-	// }
+	if configuration.Database.Migrate {
+		migrations.RunAllMigrations(db)
+		seed.SeedRolesAndPermissions(logger, db.Postgresql)
+		seed.SeedPlans(logger, db.Postgresql)
+		seed.SeedIntegrations(logger, db.Postgresql)
+		seed.SeedIndex(logger, db.Elastic)
+	}
 
 	r := router.Setup(logger, validatorRef, db, &configuration.App)
 
