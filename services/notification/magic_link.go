7a86cfa3a8742def763aa7658f84a072cc5a38fd
Ebite God'sgift Uloamaka <godsgiftuloamaka235@gmail.com>
2024-12-29 14:42:30 +0100

    fix: email external links

--- services/notification/magic_link.go
+++ services/notification/magic_link.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/hngprojects/telex_be/internal/config"
 	"github.com/hngprojects/telex_be/internal/models"
 	"github.com/hngprojects/telex_be/services/send"
 )
@@ -16,21 +17,30 @@ func (n NotificationObject) SendMagicLink() error {
 		baseTemplateFileName = ""
 		errs                 []string
 		user                 models.User
+		configData           = config.GetConfig()
 	)
+	subject := "Subject: Secure Login: Your MagicLink..."
+	loginUrl := fmt.Sprintf("%v/login", configData.App.FRONTEND_URL)
+	contactUrl := fmt.Sprintf("%v/contact", configData.App.FRONTEND_URL)
+	policyUrl := fmt.Sprintf("%v/policy", configData.App.FRONTEND_URL)
 
 	err := json.Unmarshal([]byte(n.Notification.Data), &notificationData)
 	if err != nil {
 		return fmt.Errorf("error decoding saved notification data, %v", err)
 	}
 
-	subject := "Subject: Secure Login: Your MagicLink..."
-
 	user, err = user.GetUserByEmail(n.Db, notificationData.Email)
 	if err != nil {
 		return fmt.Errorf("error getting user with account id %v, %v", notificationData.Email, err)
 	}
 
-	data, err := ConvertToMapAndAddExtraData(notificationData, map[string]interface{}{"firstname": thisOrThatStr(user.Profile.FirstName, user.Email), "business_name": thisOrThatStr("", "")})
+	data, err := ConvertToMapAndAddExtraData(notificationData, map[string]interface{}{
+		"firstname": thisOrThatStr(user.Profile.FirstName, user.Email),
+		"business_name": thisOrThatStr("", ""),
+		"login_url": loginUrl,
+		"contact_us_url": contactUrl,
+		"privacy_policy_url": policyUrl,
+		})
 	if err != nil {
 		return fmt.Errorf("error converting data to map, %v", err)
 	}
