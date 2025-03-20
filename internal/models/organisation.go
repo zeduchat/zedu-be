772d8cb020cc3c6cec288dbd02586a7f6dff4fc3
Micah Shallom Bawa <micahshallom@gmail.com>
2025-03-20 19:02:45 +0100

    chore: fixed username not showing on users fetch

--- internal/models/organisation.go
+++ internal/models/organisation.go
@@ -60,6 +60,7 @@ type UpdateOrgRequestModel struct {
 type UserInOrgResponse struct {
 	ID          string    `json:"id"`
 	Email       string    `json:"email"`
+	UserName    string    `json:"user_name"`
 	PhoneNumber string    `json:"phone_number"`
 	AvatarURL   string    `json:"profile_url"`
 	Name        string    `json:"name"`
