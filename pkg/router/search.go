f95562bd395e58d40d01f77244f77a896327f83c
kehl-gopher <kelanidarasimi9@gmail.com>
2025-03-22 14:33:27 +0100

    fix: fetch userId from access_token

--- pkg/router/search.go
+++ pkg/router/search.go
@@ -17,7 +17,7 @@ func Search(r *gin.Engine, ApiVersion string, validator *validator.Validate, db 
 	search := search.Controller{Db: db, Validator: validator, Logger: logger, ExtReq: extReq}
 	searchUrl := r.Group(fmt.Sprintf("%v", ApiVersion), middleware.Authorize(db.Postgresql))
 	{
-		searchUrl.GET("/search/organisation/:orgId/user/:userId/", search.Search)
+		searchUrl.GET("/search/organisation/:orgId/", search.Search)
 	}
 	return r
 }
