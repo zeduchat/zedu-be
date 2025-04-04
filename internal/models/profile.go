79a77ab6b2aca790f4b9a430cac92ca2f383452b
cyberguru <hamzacypher34@gmail.com>
2025-04-04 06:56:15 -0500

    Chore: Fix Feild typo

--- internal/models/profile.go
+++ internal/models/profile.go
@@ -57,7 +57,7 @@ type UpdateUserProfileRequest struct {
 	AvatarURL         string `json:"avatar_url"`
 	DisplayName       string `json:"display_name"`
 	Title             string `json:"title"`
-	NamePronunciation string `json:"name_pronunciation"`
+	NamePronunciation string `json:"name_pronounciation"`
 	Timezone          string `json:"timezone"`
 }
 
