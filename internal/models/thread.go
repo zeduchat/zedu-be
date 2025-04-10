fb498a2112a0815251c1247d455cdc7c22dcee81
Tonyrealzy <umehobiarinze2@gmail.com>
2025-04-10 21:51:59 +0100

    fix: Changed the query for getting messages and media items in threads

--- internal/models/thread.go
+++ internal/models/thread.go
@@ -526,15 +526,26 @@ func (c *Threads) UpdateThread(db *gorm.DB, req map[string]interface{}) (*Thread
 
 func (c *Threads) DeleteThread(db *gorm.DB) (*Threads, error) {
 
-	query := map[string]interface{}{
+	mediaQuery := map[string]interface{}{
 		"query": map[string]interface{}{
-			"match": map[string]interface{}{
-				"thread_id": c.ID,
+			"nested": map[string]interface{}{
+				"path": "messages.media", // Access the media field within messages
+				"query": map[string]interface{}{
+					"bool": map[string]interface{}{
+						"must": []interface{}{
+							map[string]interface{}{
+								"match": map[string]interface{}{
+									"messages.thread_id": c.ID, // Match messages that belong to this thread
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
 
-	err := elastic.DeleteByQuery(storage.DB.Elastic, MessageIndexName, query)
+	err := elastic.DeleteByQuery(storage.DB.Elastic, ThreadIndexName, mediaQuery)
 
 	if err != nil {
 		return nil, fmt.Errorf("failed to delete thread messages, err: %v", err)
@@ -547,6 +558,30 @@ func (c *Threads) DeleteThread(db *gorm.DB) (*Threads, error) {
 
 	return c, nil
 }
+
+// func (c *Threads) DeleteThread(db *gorm.DB) (*Threads, error) {
+
+// 	query := map[string]interface{}{
+// 		"query": map[string]interface{}{
+// 			"match": map[string]interface{}{
+// 				"thread_id": c.ID,
+// 			},
+// 		},
+// 	}
+
+// 	err := elastic.DeleteByQuery(storage.DB.Elastic, MessageIndexName, query)
+
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to delete thread messages, err: %v", err)
+// 	}
+
+// 	err = elastic.DeleteDocument(storage.DB.Elastic, ThreadIndexName, c.ID)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("Invalid thread uuid supplied")
+// 	}
+
+// 	return c, nil
+// }
 
 func (c *Threads) DeleteThreadMediaFiles(logger *utility.Logger, db *gorm.DB, mediaFiles []UploadedFileResponse) (*Threads, error) {
 	for _, mediaFile := range mediaFiles {
