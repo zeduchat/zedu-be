79e778407d3fd0223bcf50f8ad5001643d679ee1
Tonyrealzy <umehobiarinze2@gmail.com>
2025-04-12 12:42:26 +0100

    Fix: Edited the model to allow deletion of media files in thread documents.

--- internal/models/thread.go
+++ internal/models/thread.go
@@ -545,7 +545,10 @@ func (c *Threads) DeleteThreadMediaFiles(logger *utility.Logger, db *gorm.DB, me
 func (c *Threads) DeleteThread(logger *utility.Logger, db *gorm.DB, threadDoc ThreadDocument) (*Threads, error) {
 
 	for _, message := range threadDoc.Messages {
-		c.DeleteThreadMediaFiles(logger, db, message.Media)
+		_, err := c.DeleteThreadMediaFiles(logger, db, message.Media)
+		if err != nil {
+			return nil, fmt.Errorf("failed to delete media files in thread document, err: %v", err)
+		}
 	}
 
 	messageQuery := map[string]interface{}{
