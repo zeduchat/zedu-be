395258e12cd1d1c20005da5c8118df6ec4477733
cyberguru <hamzacypher34@gmail.com>
2025-09-22 11:00:31 -0500

    Chore: updated swagger docs

--- internal/models/agents.go
+++ internal/models/agents.go
@@ -81,7 +81,7 @@ type PublishAgentRequest struct {
 	HowItWorks       string    `json:"how_it_works" validate:"required,min=101"`
 	Benefits         string    `json:"benefits" validate:"required,min=101"`
 	WhyUse           string    `json:"why_use" validate:"required,min=101"`
-	ShortDescription string    `json:"short_description" validate:"required,min=10"`
+	ShortDescription string    `json:"short_description" validate:"required,min=10,max=30"`
 	LongDescription  string    `json:"long_description" validate:"required,min=101"`
 	UserId           string    `json:"-"`
 	AgentId          string    `json:"-"`
