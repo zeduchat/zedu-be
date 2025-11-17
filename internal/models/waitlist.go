9c0fe0886fcfc76006e917febd4c51b6179b5496
cyberguru <hamzacypher34@gmail.com>
2025-11-17 08:08:05 -0600

    Chore: fix bug with router

--- internal/models/waitlist.go
+++ internal/models/waitlist.go
@@ -11,7 +11,8 @@ import (
 
 type Waitlist struct {
 	ID        string         `gorm:"primaryKey;type:uuid" json:"id"`
-	Email     string         `gorm:"unique;not null" json:"email" validate:"required,email"`
+	Email     string         `gorm:"unique;not null;column:email" json:"email" validate:"required,email"`
+	Name      string         `gorm:"column:name" json:"name"`
 	CreatedAt time.Time      `gorm:"column:created_at; not null; autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time      `gorm:"column:updated_at; null; autoUpdateTime" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -62,7 +62,7 @@ func Setup(logger *utility.Logger, validator *validator.Validate, db *storage.Da
 	GroupDMs(r, ApiVersion, validator, db, logger)
 	Group(r, ApiVersion, validator, db, logger)
 	Admin(r, ApiVersion, validator, db, logger)
-
+	Waitlist(r, ApiVersion, validator, db, logger)
 	Search(r, ApiVersion, validator, db, logger)
 	Agents(r, ApiVersion, validator, db, logger)
 	Mongogrations(r, ApiVersion, validator, db, logger)
