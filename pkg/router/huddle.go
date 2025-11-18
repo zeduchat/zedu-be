aae84d2a1085db7f86428d9005d92878007fa054
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-11-18 17:39:12 +0100

    feat: add huddle routes endpoints

---
+++ pkg/router/huddle.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+
+	"github.com/hngprojects/telex_be/pkg/controller/huddle"
+	"github.com/hngprojects/telex_be/pkg/middleware"
+	"github.com/hngprojects/telex_be/pkg/repository/storage"
+	"github.com/hngprojects/telex_be/utility"
+)
+
+func Huddles(r *gin.Engine, ApiVersion string, validator *validator.Validate, db *storage.Database, logger *utility.Logger) *gin.Engine {
+	ctrl := huddle.Controller{
+		Db: db, 
+		Validator: validator, 
+		Logger: logger,
+	}
+
+	huddleGroup := r.Group(fmt.Sprintf("%v/huddles", ApiVersion), middleware.Authorize(db.Postgresql), middleware.CheckIsDeactivated(db.Postgresql))
+	{
+		huddleGroup.POST("/create", ctrl.Create)
+	}
+
+	return r
+}
