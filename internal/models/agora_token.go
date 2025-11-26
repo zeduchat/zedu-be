739d5de61d314aa802a3d07ccf7ad0a97d3a4768
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-11-26 11:48:13 +0100

    feat: add device token handler to router

--- internal/models/agora_token.go
+++ internal/models/agora_token.go
@@ -1,12 +1,12 @@
 package models
 
-// AgoraTokenRequest is the request to get an Agora RTC token
-type AgoraTokenRequest struct {
+// HuddleAgoraTokenRequest is the request to get an Agora RTC token for a huddle
+type HuddleAgoraTokenRequest struct {
 	HuddleID string `json:"huddle_id" validate:"required,uuid"`
 }
 
-// AgoraTokenResponse contains the Agora RTC token and connection details
-type AgoraTokenResponse struct {
+// HuddleAgoraTokenResponse contains the Agora RTC token and connection details for a huddle
+type HuddleAgoraTokenResponse struct {
 	Token       string `json:"token"`
 	AppId       string `json:"app_id"`
 	ChannelName string `json:"channel_name"`
--- pkg/repository/agora/agora.go
+++ pkg/repository/agora/agora.go
@@ -67,8 +67,8 @@ func (s *AgoraService) GenerateRTCToken(channelName, userID string, expireTimeIn
 }
 
 // GetAgoraToken generates an Agora RTC token for a user to join a huddle
-func GetAgoraToken(db *storage.Database, logger *utility.Logger, huddleID, userID string) (models.AgoraTokenResponse, int, error) {
-	var resp models.AgoraTokenResponse
+func GetAgoraToken(db *storage.Database, logger *utility.Logger, huddleID, userID string) (models.HuddleAgoraTokenResponse, int, error) {
+	var resp models.HuddleAgoraTokenResponse
 
 	service := Client.Service
 	if service == nil {
@@ -97,7 +97,7 @@ func GetAgoraToken(db *storage.Database, logger *utility.Logger, huddleID, userI
 		return resp, http.StatusInternalServerError, errors.New("failed to generate access token")
 	}
 
-	resp = models.AgoraTokenResponse{
+	resp = models.HuddleAgoraTokenResponse{
 		Token:       token,
 		AppId:       service.appId,
 		ChannelName: huddleID,
