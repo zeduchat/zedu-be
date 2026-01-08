7e8dc89fc81db795e38f041eb05262f05f411d2b
berylatieno <berylatieno30@gmail.com>
2026-01-08 16:26:37 +0300

    add user activity and signin columns

---
+++ db/migrations/000034_add_user_activity_and_signin.down.sql
@@ -0,0 +1,7 @@
+@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_users_last_activity_at;
+DROP INDEX IF EXISTS idx_users_last_log_in_at;
+
+ALTER TABLE users
+  DROP COLUMN IF EXISTS last_activity_at;
+  DROP COLUMN IF EXISTS last_log_in_at;
