fd28bd861db036a2fba26256d23d288dd58dc881
cyberguru <hamzacypher34@gmail.com>
2026-01-19 11:28:12 -0600

    Chore: updated go auto migrate

--- internal/models/profile.go
+++ internal/models/profile.go
@@ -21,7 +21,7 @@ type Profile struct {
 	UserName          string         `gorm:"column:user_name; type:text;" json:"username"`
 	Phone             string         `gorm:"type:varchar(255)" json:"phone"`
 	AvatarURL         string         `gorm:"type:varchar(255)" json:"avatar_url"`
-	Userid            string         `gorm:"type:uuid;uniqueIndex:idx_profile_userid" json:"user_id"`
+	Userid            string         `gorm:"type:uuid;unique" json:"user_id"`
 	CreatedAt         time.Time      `gorm:"column:created_at; not null; autoCreateTime" json:"created_at"`
 	UpdatedAt         time.Time      `gorm:"column:updated_at; null; autoUpdateTime" json:"updated_at"`
 	DeletedAt         gorm.DeletedAt `gorm:"index" json:"-"`
