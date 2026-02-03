55a0a9dc50ed78025504f2b1b94ef2517a49408b
Shallom Micah Bawa <micahshallom@gmail.com>
2026-02-03 07:56:45 +0100

    cleanup: removed seed superadmin

--- main.go
+++ main.go
@@ -101,7 +101,6 @@ func main() {
 		seed.SeedIndex(logger, db.Elastic)
 		seed.SeedCreditPackages(logger, db.Postgresql)
 		seed.SeedTranslatorPrompt(logger, db.Postgresql)
-		seed.SeedSuperAdmin(logger, db.Postgresql)
 	}
 
 	r := router.Setup(logger, validatorRef, db, &configuration.App)
