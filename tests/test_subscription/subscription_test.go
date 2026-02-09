5d638df8d5ae67393279b3c212676f2830632eff
cyberguru <hamzacypher34@gmail.com>
2026-02-09 06:21:45 -0600

    Chore: updated Get Sub with no duplicates

--- tests/test_subscription/subscription_test.go
+++ tests/test_subscription/subscription_test.go
@@ -44,16 +44,12 @@ func TestGetSubscriptions_NoDuplicates(t *testing.T) {
 	logger := tst.Setup()
 	db := storage.Connection()
 
-	db.Postgresql.Exec("DELETE FROM org_roles WHERE 1 = 1")
 	db.Postgresql.Unscoped().Where("1 = 1").Delete(&models.OrganisationPlan{})
 	db.Postgresql.Unscoped().Where("1 = 1").Delete(&models.Plan{})
 
 	if err := db.Postgresql.AutoMigrate(&models.Plan{}, &models.Organisation{}, &models.OrganisationPlan{}); err != nil {
 		t.Fatalf("Migration failed: %v", err)
 	}
-
-	seed.SeedPlans(logger, db.Postgresql)
-	seed.SeedRolesAndPermissions(logger, db.Postgresql)
 
 	var plan models.Plan
 	err := db.Postgresql.Where("name = ?", "Free").First(&plan).Error
