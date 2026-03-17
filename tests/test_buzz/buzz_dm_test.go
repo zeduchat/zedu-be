61156651383e1b00967a1899f4a1bd6765981206
cyberguru <hamzacypher34@gmail.com>
2026-03-17 06:17:12 -0500

    Chore: updated failing test case

--- tests/test_buzz/buzz_dm_test.go
+++ tests/test_buzz/buzz_dm_test.go
@@ -251,11 +251,11 @@ func TestBuzzCreateInDM(t *testing.T) {
 		rr := httptest.NewRecorder()
 		r.ServeHTTP(rr, req)
 
-		tst.AssertStatusCode(t, rr.Code, http.StatusOK)
+		tst.AssertStatusCode(t, rr.Code, http.StatusCreated)
 
 		data := tst.ParseResponse(rr)
 		code := int(data["status_code"].(float64))
-		tst.AssertStatusCode(t, code, http.StatusOK)
+		tst.AssertStatusCode(t, code, http.StatusCreated)
 
 		message := data["message"].(string)
 		tst.AssertResponseMessage(t, message, "buzz created successfully")
